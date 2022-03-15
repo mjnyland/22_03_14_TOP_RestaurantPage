c7398bf5ccae753e127288b78cee0cca5a1630f6
mjnyland <mjnyland@gmail.com>
2022-03-15 07:39:17 -0500

    Organizing HTML

---
+++ src/home.js
@@ -0,0 +1,7 @@
+const Showtime = (title, theater, runtime, format) => {
+    return{title, theater, runtime, format}
+}
+
+const theWorstPers = Showtime('THE WORST PERSON IN THE WORLD', 'THEATER 1', '160 MINS', '35MM')
+
+export default{theWorstPers}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('hello world!')
+import theWorstPers from './home.js'
+
+console.log(theWorstPers)
